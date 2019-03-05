0f570787884590df2b311b9f31c1a863431eb1ba
dvjeshko <r.bochkarev@bk.ru>
2019-03-05 17:50:10 +0300

    конфиг

--- src/config/default.config.js
+++ src/config/default.config.js
@@ -1,5 +1,6 @@
 const config = {
-    api_server:'http://rbcall-rbcall-3212.cloud4.newrobocall.ru/app/'
+    api_server:'http://rbcall-rbcall-3212.cloud4.newrobocall.ru/app/',
+    api_websocket:'http://newrobocall.ru:3000'
 }
 
 export default config
