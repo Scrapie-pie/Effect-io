93f33428a48fe0932a089e84467a594adca29510
bagirov <xbagir@gmail.com>
2019-05-08 09:21:18 +0300

    RBCALL-3523

--- src/main.js
+++ src/main.js
@@ -23,16 +23,19 @@ import VueSocketio from 'vue-socket.io-extended';
 import io from 'socket.io-client';
 
 import config from "@/config/index";
+
 Vue.use(VueSocketio, io(config.api_websocket,{
     autoConnect: false,
     reconnection: true, // (Boolean) whether to reconnect automatically (false)
-    reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
+    reconnectionAttempts: 30, // (Number) number of reconnection attempts before giving up (Infinity),
     reconnectionDelay: 3000,
+    transports: ['websocket'],
+    query: {
+        user: 'employee'
+    }
 }), {
     store,
     actionPrefix: 'socket',
-
-
 });
 
 import BaseIcon from "@/components/BaseIcon";
