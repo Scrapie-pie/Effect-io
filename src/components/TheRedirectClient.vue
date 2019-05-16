56ed8f0053939182e20b04b3d5b4dab45e809842
dvjeshko <r.bochkarev@bk.ru>
2019-05-16 10:24:36 +0300

    ФИЧА. Добавить поле для перевода клиента на конкретный урл

---
+++ src/components/TheRedirectClient.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+    form.the-redirect-client(@submit.prevent="submit" v-if="viewModeChat==='visitors'")
+        fieldset
+            legend Перенаправление клиента
+            base-field.the-redirect-client__base-field(placeholder="Введите URL" name="redirect" v-model="model")
+            base-btn(type="submit" color="info" size="sm" padding="xs") Перенаправить
+</template>
+
+<script>
+    import { viewModeChat } from '@/mixins/mixins'
+export default {
+    name: "the-redirect-client",
+    mixins:[viewModeChat],
+    data() {
+        return {
+            model:''
+        }
+    },
+    methods:{
+        submit(){
+            this.model='';
+            console.log('submit');
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .the-redirect-client{
+        $color_bg:glob-color('info-light');
+        background-color:$color_bg;
+        padding:calc-em(15);
+        border-radius:$glob-border-radius_field;
+
+        &__base-field {
+            .field__input {
+                background-color:glob-color('light');
+                margin:calc-em(10) 0;
+            }
+
+        }
+    }
+</style>
