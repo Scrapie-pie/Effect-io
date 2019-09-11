8dc2fa978a363fa681ea92b738d9a350984b8e8a
dvjeshko <r.bochkarev@bk.ru>
2019-09-11 19:23:23 +0300

    линки для шторма

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    resolve: {
+        // for WebStorm
+        alias: {
+            '@': path.resolve(__dirname)+'/src/',
+            '~': path.resolve(__dirname)+'/src/'
+        }
+    }
+};
