a64938c8d9e4b7f43ae4a23aff3f45e6cd61ca64
dvjeshko <r.bochkarev@bk.ru>
2019-11-08 12:19:16 +0300

    transports: ['websocket','polling'],

--- src/main.js
+++ src/main.js
@@ -43,7 +43,7 @@ Vue.use(
         reconnection: true, // (Boolean) whether to reconnect automatically (false)
         reconnectionAttempts: 30, // (Number) number of reconnection attempts before giving up (Infinity),
         reconnectionDelay: 3000,
-        transports: ['polling','websocket'],
+        transports: ['websocket','polling',],
         upgrade: false,
         secure: true,
         query: {
