7a47460584ab23cb557a5739ce1d9aa4af796280
dvjeshko <r.bochkarev@bk.ru>
2020-01-13 21:21:47 +0300

    jwt log sentry

--- src/modules/sentry.js
+++ src/modules/sentry.js
@@ -8,7 +8,7 @@ import * as Integrations from '@sentry/integrations'
 if (process.env.VUE_APP_SENTRY) {
     Sentry.init({
         dsn: 'https://982fb7eca3a34dbb85ebbe1b37ad7b6a@sentry.effect.com/3',
-        integrations: [new Integrations.Vue({ Vue, attachProps: true, logErrors: true })],
+        integrations: [new Integrations.Vue({ Vue, attachProps: true, logErrors: false })],
         environment: process.env.NODE_ENV
         //debug: process.env.NODE_ENV === 'development' ? true : false
     })
