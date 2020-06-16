67342be9751ca5e50e729a0e82aaaa3fc5248fea
dvjeshko <r.bochkarev@bk.ru>
2020-06-16 12:57:52 +0300

    fix pageMonitoring

--- src/pages/PageMonitoring.vue
+++ src/pages/PageMonitoring.vue
@@ -18,8 +18,8 @@
                     td {{branch_active}}
 
                     td {{branch_day_active}}
-                tr.page-monitoring__tr(v-for="(item, index) in itemList", :key="item.user_id")
-                    td.page-monitoring__name {{item.name}}
+                tr.page-monitoring__tr(v-for="(item, index) in itemList", :key="item.id")
+                    td.page-monitoring__name {{item.fullName}}
 
                     td {{item.users_active}}
 
@@ -49,19 +49,12 @@ export default {
             branch_unprocessed:0,
             branch_active:0,
             branch_day_active:0,
+            users_active:[],
+            users_day_active:[],
 
-
-            itemList:[]
         }
     },
-    computed: {
-        count(){
-            return 1
-        },
 
-
-
-    },
 
     created() {
         this.$root.$on('emitMonitoring',this.setData)
@@ -91,28 +84,37 @@ export default {
     beforeCreate(){
         this.$root.$off('emitMonitoring',this.setData)
     },
+    computed:{
+        itemList(){
+            return this.$store.getters['operators/all'].map(operator=>{
+
+                let users_activeFind = this.users_active.find(users_activeItem=>users_activeItem.user_id===operator.id)
+                if(users_activeFind) {
+                    operator.users_active = users_activeFind.count
+                } else{
+                    operator.users_active = 0
+                }
+                let users_day_activeFind = this.users_day_active.find(users_day_activeItem=>users_day_activeItem.user_id===operator.id)
+                if(users_day_activeFind) {
+                    operator.users_day_active = users_day_activeFind.count
+                } else{
+                    operator.users_day_active = 0
+                }
+                return operator
+            })
+        }
+    },
     methods: {
         setData(r){
             this.branch_unprocessed = r.branch_unprocessed
             this.branch_active = r.branch_active
             this.branch_day_active = r.branch_day_active
 
-            this.itemList=r.users_active.map(({user_id,count},index)=>{
+            this.users_active=r.users_active
+            this.users_day_active=r.users_day_active
 
 
-                let operator = this.$store.getters['operators/all'].find(
-                    itemSub => itemSub.id === user_id
-                )
 
-                if (operator) return {
-                    user_id,
-                    name:operator.fullName,
-                    users_active:count,
-                    users_day_active:r.users_day_active[index].count
-                }
-                else return {}
-
-            })
         }
     }
 }
